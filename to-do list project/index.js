3811bafb199cf13c15a73a70472c78788f70b412
ryan <concepting@protonmail.com>
2024-05-05 16:08:28 -0400

    updates

--- to-do list project/index.js
+++ to-do list project/index.js
@@ -11,20 +11,31 @@ renderTodoList();
 function renderTodoList(){
     let todoListHTML = '';
 
-    for(let i = 0; i < todoList.length; i++){
-        const todoObject = todoList[i];
-        // const name = todoObject.name;
-        // const dueDate = todoObject.dueDate;
-
+    todoList.forEach(function(todoObject, index){
         const { name, dueDate } = todoObject;
 
         const html = `
                      <div>${name}</div>
                      <div>${dueDate}</div>
-                     <button onclick="todoList.splice(${i}, 1);renderTodoList();" class = "delete-todo-button">Delete</button>
+                     <button onclick="todoList.splice(${index}, 1);renderTodoList();" class = "delete-todo-button">Delete</button>
                      `;
         todoListHTML += html;
-    }
+    });
+
+    // for(let i = 0; i < todoList.length; i++){
+    //     const todoObject = todoList[i];
+    //     // const name = todoObject.name;
+    //     // const dueDate = todoObject.dueDate;
+
+    //     const { name, dueDate } = todoObject;
+
+    //     const html = `
+    //                  <div>${name}</div>
+    //                  <div>${dueDate}</div>
+    //                  <button onclick="todoList.splice(${i}, 1);renderTodoList();" class = "delete-todo-button">Delete</button>
+    //                  `;
+    //     todoListHTML += html;
+    // }
     console.log(todoListHTML);
 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
