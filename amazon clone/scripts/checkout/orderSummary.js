781685dac8a810ae0a2259528b50339d7070f04c
ryan <concepting@protonmail.com>
2024-05-07 13:03:21 -0400

    make payment summary interactive

--- amazon clone/scripts/checkout/orderSummary.js
+++ amazon clone/scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 hello();
 
@@ -122,6 +123,8 @@ cart.forEach((cartItem) => {
             
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+
+            renderPaymentSummary();
         });
     });
 
@@ -130,6 +133,8 @@ cart.forEach((cartItem) => {
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+
+            renderPaymentSummary();
         });
     });
 }
