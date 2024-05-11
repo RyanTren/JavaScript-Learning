6bae6dad06147598019063bddf0d594a6edec461
ryan <concepting@protonmail.com>
2024-05-11 00:06:04 -0400

    convert from promises to async awaits

--- amazon clone/scripts/checkout.js
+++ amazon clone/scripts/checkout.js
@@ -6,6 +6,24 @@ import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+async function loadPage(){
+
+    await loadProductsFetch();
+
+    await new Promise((resolve) =>{
+        loadCart(() => {
+            resolve('value3');
+        });
+    });
+
+    renderOrderSummary();
+    renderPaymentSummary();
+
+}
+loadPage();
+
+
+/*
 Promise.all([
     loadProductsFetch(),
     new Promise((resolve) =>{
@@ -19,6 +37,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
